b8fe8af7a3b6880c7315061dad026cc608781c2b
dealonzo <denisca86@gmail.com>
2022-07-31 21:21:10 -0600

    trying with onClick event

--- src/Content.js
+++ src/Content.js
@@ -1,20 +1,34 @@
 import React from 'react'
 
-type Props = {}
-
-const Content = (props: Props) => {
+const Content = () => {
 
     const handleChangeName = () => {
         const names = ['Dave','Martin','Louis']
         const random = Math.floor(Math.random() * 3)
+        console.log(`--> ${names[random]}`)
         return names[random]
-        }
+    }
+
+    const handleClick = () => {
+        console.log('Button has been clicked!')
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+    }
+
+    const handleClick3 = (e) => {
+        console.log(e.target)
+    }
 
     return (
         <main>
             <p>
             Hello {handleChangeName()}!
             </p>
+            <button onClick={handleClick}>Click Me!</button>
+            <button onClick={() => handleClick2('Denis')}>Click Me!</button>
+            <button onClick={(e) => handleClick3(e)}>Click Me!</button>
         </main>
     )
 }
